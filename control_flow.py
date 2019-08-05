cf0037fba02d6304932a65a19c0fcef6c9720131
johnguerra <johnmguerra@gmail.com>
2019-08-05 11:06:29 -0600

    added nesting

--- control_flow.py
+++ control_flow.py
@@ -7,5 +7,19 @@ Learn about control flow in Python
 # Indentation is 4 spaces
 if True:
     print("It is true")
+if bool("eggs"):
+    print("Yes, why not")
 
+# Flat is better than Nested
+h = 42
+if h > 50:
+    print("Greater than 50")
+    if h > 100
+        print("Greater than 100")
+elif h < 50:
+    print("Less than 50")
+else:
+    print("Equals 50")
+
+########
 print("Done")
