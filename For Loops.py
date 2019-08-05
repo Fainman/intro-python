4a0837e4a377138a8b5a37ddc8358ebdd7750bab
johnguerra <johnmguerra@gmail.com>
2019-08-05 14:10:31 -0600

    Working with lists

---
+++ For Loops.py
@@ -0,0 +1,16 @@
+"""
+
+Learning For Loops
+"""
+
+cities = ["London", "New York", "Paris", "Oslo", "Helsinki"]
+
+for city in cities:
+    print(city)
+
+d = {'alice': '878-8728-922',
+     'bob': '256-5262-124',
+     'eve': '198-2321-787'}
+
+for item in d:
+    print(item, "=>", d[item])
