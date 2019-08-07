7a3e4819ffb216b18dfa5508db2e20b0de457d7a
johnguerra <johnmguerra@gmail.com>
2019-08-07 14:46:06 -0600

    Exploring exception

--- my_exceptions.py
+++ my_exceptions.py
@@ -20,14 +20,39 @@ def convert(s):
     return -1
 
 
+def sqrt(x):
+    """
+
+    :param x:
+    :return:
+    :raise ValueError() on ZeroDivisionError
+    """
+    guess = x
+    i = 0
+    try:
+        while guess*guess != x and i < 20:
+            guess = (guess + x/guess)/2.0
+            i += 1
+    except ZeroDivisionError:
+        raise ValueError()
+    return guess
+
+
 def main():
     """
     Test function
     :return: 
     """
-    print(convert('11'))
-    print(convert('Hello'))
-    print(convert([1, 4, 8]))
+    # print(convert('11'))
+    # print(convert('Hello'))
+    # print(convert([1, 4, 8]))
+    try:
+        print(sqrt(9))
+        print(sqrt(11))
+        print(sqrt(-1))
+    except ValueError:
+        print("Cannot compute the value of negative numbers")
+    print("Done")
 
 
 if __name__ == "__main__":
