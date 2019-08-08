74e9644dac3e39e12e4366d2328736a2c5dd6474
johnguerra <johnmguerra@gmail.com>
2019-08-08 16:04:21 -0600

    Add seat allocation function

--- use_airtravel.py
+++ use_airtravel.py
@@ -5,12 +5,7 @@ from pprint import pprint as pp
 from airtravel import Flight, Aircraft
 
 
-def main():
-    """
-    Test function
-    :return: 
-    """
-
+def make_flight():
     a1 = Aircraft("G-EUP", "Airbus A319",
                   num_rows=22,
                   num_seats_per_row=6)
@@ -19,10 +14,37 @@ def main():
     print(f1.number(), f1.airline())
     print(a1.registration(), a1.model())
     print(a1.seating_plan())
-    pp(f1._seating)
     f1.allocate_seat('12C', 'John')
     f1.allocate_seat('12D', 'James')
-    pp(f1._seating)
+    return f1
+
+
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = "| Name: {0}" \
+            "Flight: {1} " \
+            "Seat: {2}"    \
+            "Aircraft: {3}"\
+            " |".format(passenger, flight_number, seat, aircraft)
+    banner = "+" + "-" * (len(output)-2) + "+"
+    border = "|" + " " * (len(output)-2) + "|"
+    lines = [banner, border, output, border, banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()
+
+
+def main():
+    """
+    Test function
+    :return: 
+    """
+    flight_1 = make_flight()
+    pp(flight_1._seating)
+    print("Available seats",
+          flight_1.num_available_seats())
+    flight_1.make_boarding_class(
+        console_card_printer)
+    print()
 
 
 if __name__ == '__main__':
