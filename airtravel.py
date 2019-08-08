4e03cabd42b6cc8a6bb12d00309e78d4b7430044
johnguerra <johnmguerra@gmail.com>
2019-08-08 15:05:30 -0600

    Add seat allocation function

--- airtravel.py
+++ airtravel.py
@@ -1,13 +1,11 @@
 """
 A Flight Class. Model for aircraft flights
 """
-
-
 class Flight:
     """
     A Flight wit ha particular passenger aircraft
     """
-    def __init__(self, number):
+    def __init__(self, number, aircraft):
         # Validate flight number:
         # 5 chars long: AADDD A-> Alpha, D->Digit
         if len(number) != 5:
@@ -18,7 +16,12 @@ class Flight:
             raise ValueError("Invalid flight number. First two must be uppercase".format(number))
         if not number[3:].isdecimal():
             raise ValueError("Invalid flight number. Last three must be numeric".format(number))
+
         self._number = number  # implementation details begin with '_'
+        self._aircraft = aircraft
+
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
 
     def number(self):
         """
@@ -34,20 +37,55 @@ class Flight:
         """
         return self._number[:2]
 
+    def allocate_seat(self, seat, passenger):
+        """
+        Allocate a seat to passenger
+        :param seat: A seat designator such as '12C'
+        :param passenger: The passenger name
+        :return:
+        """
+        rows, seat_letter = self._aircraft.seating_plan()
+
+        letter = seat[-1]  # take the letter from the seat
+        if letter not in seat_letter:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row {}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number{}".format(row))
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+        self._seating[row][letter] = passenger
+
+
 
 class Aircraft:
-        def __init__(self, registration, model, num_rows,
-                     num_seats_per_row):
-            self._registration = registration
-            self._model = model
-            self._num_rows = num_rows
-            self._num_seats_per_row = num_seats_per_row
+    def __init__(self, registration, model, num_rows,
+                 num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
 
-        def registration(self):
-            return self._registration
 
-        def model(self):
-            return self._model
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return(range(1, self._num_rows + 1),\
+               "ABCDEFGHJK"[:self._num_seats_per_row])
+
+
+
+
 
 
 def main():
--- use_airtravel.py
+++ use_airtravel.py
@@ -1,7 +1,7 @@
 """
 Use flight class
 """
-
+from pprint import pprint as pp
 from airtravel import Flight, Aircraft
 
 
@@ -10,18 +10,19 @@ def main():
     Test function
     :return: 
     """
-    f = Flight("SN066")
-    print(f, type(f))
-    print(f.number(), f.airline())
-    # Because we use self, don't need Flight.number(f)
-
-    f2 = Flight("SN063")
-    print(f2.number(), f2.airline())
 
     a1 = Aircraft("G-EUP", "Airbus A319",
                   num_rows=22,
                   num_seats_per_row=6)
+
+    f1 = Flight("SN063", a1)
+    print(f1.number(), f1.airline())
     print(a1.registration(), a1.model())
+    print(a1.seating_plan())
+    pp(f1._seating)
+    f1.allocate_seat('12C', 'John')
+    f1.allocate_seat('12D', 'James')
+    pp(f1._seating)
 
 
 if __name__ == '__main__':
